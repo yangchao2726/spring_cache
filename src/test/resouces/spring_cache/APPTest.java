7944acbd6408f0d15547c257a0dbcd8e46c58a64
yangchao2726 <yangchao2726@163.com>
2016-12-28 10:38:31 +0800

    测试路径

---
+++ src/test/resouces/spring_cache/APPTest.java
@@ -0,0 +1,27 @@
+/**
+ * @Description:TODO
+ * @author:YC
+ * @time:2016年12月28日 上午10:37:16
+ */
+package spring_cache;
+
+/**
+ * @Description:TODO
+ * @author:YC
+ * @time:2016年12月28日 上午10:37:16
+ */
+public class APPTest {
+
+	/**
+	 * @Description:
+	 * @param args
+	 * return:void
+	 * @exception:
+	 * @author: YC
+	 * @time:2016年12月28日 上午10:37:16
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
